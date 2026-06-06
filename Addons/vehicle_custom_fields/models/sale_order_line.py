96cb5299307c76fb32cbb187c5c93840a361d293
abdelrahman <abdelrahmansayed957@gmail.com>
2026-06-06 23:59:40 +0300

    Add rental fields to sale order

--- Addons/vehicle_custom_fields/models/sale_order_line.py
+++ Addons/vehicle_custom_fields/models/sale_order_line.py
@@ -5,21 +5,21 @@ class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
     @api.depends('product_id', 'product_uom_qty', 'price_unit', 'discount',
-                 'order_id.rental_duration', 'product_id.is_vehicle')
+                 'order_id.rental_duration', 'product_id.is_vehicle', 'tax_ids')
     def _compute_amount(self):
         for line in self:
             if line.product_id.is_vehicle and line.order_id.rental_duration:
                 duration = line.order_id.rental_duration
-                price = line.price_unit * line.product_uom_qty * duration
+                unit_price = line.price_unit
                 if line.discount:
-                    price = price * (1 - line.discount / 100)
-                    taxes = line.tax_ids.compute_all(
-                    line.price_unit * duration,
+                    unit_price = unit_price * (1 - line.discount / 100)
+                taxes = line.tax_ids.compute_all(
+                    unit_price * duration,
                     line.order_id.currency_id,
                     line.product_uom_qty,
                     product=line.product_id,
                     partner=line.order_id.partner_shipping_id,
-                    )
+                )
                 line.price_subtotal = taxes['total_excluded']
                 line.price_tax = taxes['total_included'] - taxes['total_excluded']
                 line.price_total = taxes['total_included']

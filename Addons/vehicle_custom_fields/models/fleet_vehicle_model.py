dacdb8344e004662daf8a1e2f01cad51bcd5d7a2
abdelrahman <abdelrahmansayed957@gmail.com>
2026-06-12 20:48:19 +0300

    Add muk_web_appsbar

--- Addons/vehicle_custom_fields/models/fleet_vehicle_model.py
+++ Addons/vehicle_custom_fields/models/fleet_vehicle_model.py
@@ -1,7 +1,17 @@
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class FleetVehicleModel(models.Model):
     _inherit = 'fleet.vehicle.model'
 
     vehicle_image = fields.Image(string='Vehicle Image')
+
+    @api.depends('name', 'brand_id', 'model_year')
+    def _compute_display_name(self):
+        for model in self:
+            name = model.name or ''
+            if model.brand_id:
+                name = f"{model.brand_id.name}/{name}"
+            if model.model_year:
+                name = f"{name} ({model.model_year})"
+            model.display_name = name

aec84c4a71d0cc41bea9084119da36afce38879a
abdelrahman <abdelrahmansayed957@gmail.com>
2026-06-12 23:51:50 +0300

    Updates

--- Addons/vehicle_custom_fields/models/car_rental_contract.py
+++ Addons/vehicle_custom_fields/models/car_rental_contract.py
@@ -18,28 +18,10 @@ class CarRentalContract(models.Model):
 
     exit_fuel = fields.Float(string='Exit Fuel (%)')
 
-    @api.onchange('sale_order_id')
-    def _onchange_sale_order_id(self):
-        if self.sale_order_id:
-            self.customer_id = self.sale_order_id.partner_id
-            self.rent_start_date = self.sale_order_id.rental_date_from
-            self.rent_end_date = self.sale_order_id.rental_date_to
-            self.pickup_location = self.sale_order_id.pickup_location
-            self.dropoff_location = self.sale_order_id.dropoff_location
-
-
     rental_period_days = fields.Integer(
         string='Rental Period (Days)',
         compute='_compute_rental_period_days',
     )
-
-    @api.depends('rent_start_date', 'rent_end_date')
-    def _compute_rental_period_days(self):
-        for contract in self:
-            if contract.rent_start_date and contract.rent_end_date:
-                contract.rental_period_days = (contract.rent_end_date - contract.rent_start_date).days
-            else:
-                contract.rental_period_days = 0 
 
     checklist_line = fields.One2many(
         default=lambda self: self._default_checklist_line(),
@@ -50,7 +32,15 @@ class CarRentalContract(models.Model):
             ('name', 'in', ['Rent Fees', 'Pick Up Charges', 'Drop Off Charges'])
         ])
         return [(0, 0, {'name': tool.id, 'price': 0.0}) for tool in tools]
-    
+
+    @api.depends('rent_start_date', 'rent_end_date')
+    def _compute_rental_period_days(self):
+        for contract in self:
+            if contract.rent_start_date and contract.rent_end_date:
+                contract.rental_period_days = (contract.rent_end_date - contract.rent_start_date).days
+            else:
+                contract.rental_period_days = 0
+
     @api.onchange('sale_order_id')
     def _onchange_sale_order_id(self):
         if self.sale_order_id:
@@ -77,8 +67,9 @@ class CarRentalContract(models.Model):
                     if matching:
                         line.price = matching[0].price_subtotal
 
-    class CarRentalChecklist(models.Model):
-        _inherit = 'car.rental.checklist'
+
+class CarRentalChecklist(models.Model):
+    _inherit = 'car.rental.checklist'
 
     def unlink(self):
         protected_names = ['Rent Fees', 'Pick Up Charges', 'Drop Off Charges']

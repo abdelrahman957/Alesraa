427f47b6a70be2a074c644a7d73f316d260745a8
abdelrahman <abdelrahmansayed957@gmail.com>
2026-06-12 11:50:33 +0300

    Add rental fields to sale order

--- Addons/vehicle_custom_fields/models/res_partner.py
+++ Addons/vehicle_custom_fields/models/res_partner.py
@@ -5,3 +5,5 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     driving_license_no = fields.Char(string='Driving License No')
+    vat_valid_to = fields.Date(string='Valid Till')
+    driving_license_valid_to = fields.Date(string='Valid Till')

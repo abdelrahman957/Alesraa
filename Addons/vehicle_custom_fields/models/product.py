547a44f48cd0cb6eb176fc2366cbb3e127e89c5c
abdelrahman <abdelrahmansayed957@gmail.com>
2026-06-13 15:47:07 +0300

    Updates

--- Addons/vehicle_custom_fields/models/product.py
+++ Addons/vehicle_custom_fields/models/product.py
@@ -1,4 +1,11 @@
-from odoo import fields, models
+from odoo import fields, models, api
+import base64
+from io import BytesIO
+
+try:
+    from PIL import Image
+except ImportError:
+    Image = None
 
 
 class ProductTemplate(models.Model):
@@ -6,8 +13,37 @@ class ProductTemplate(models.Model):
 
     is_vehicle = fields.Boolean(string='Is Vehicle', default=False)
 
+    image_for_report = fields.Binary(
+        string='Image for Report',
+        compute='_compute_image_for_report',
+    )
+
+    @api.depends('image_1920')
+    def _compute_image_for_report(self):
+        for product in self:
+            if product.image_1920 and Image:
+                img_data = base64.b64decode(product.image_1920)
+                img = Image.open(BytesIO(img_data))
+                if img.mode in ('RGBA', 'LA', 'P'):
+                    background = Image.new('RGB', img.size, (255, 255, 255))
+                    img = img.convert('RGBA')
+                    background.paste(img, mask=img.split()[-1])
+                    img = background
+                else:
+                    img = img.convert('RGB')
+                buffer = BytesIO()
+                img.save(buffer, format='JPEG', quality=90)
+                product.image_for_report = base64.b64encode(buffer.getvalue())
+            else:
+                product.image_for_report = product.image_1920
+
 
 class ProductProduct(models.Model):
     _inherit = 'product.product'
 
     is_vehicle = fields.Boolean(string='Is Vehicle', related='product_tmpl_id.is_vehicle', store=True)
+
+    image_for_report = fields.Binary(
+        string='Image for Report',
+        related='product_tmpl_id.image_for_report',
+    )

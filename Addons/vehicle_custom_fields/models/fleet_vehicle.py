53c64d39b7011d945e79aad6072318c5a90272fd
abdelrahman <abdelrahmansayed957@gmail.com>
2026-06-17 00:57:27 +0300

    Updates

--- Addons/vehicle_custom_fields/models/fleet_vehicle.py
+++ Addons/vehicle_custom_fields/models/fleet_vehicle.py
@@ -102,6 +102,12 @@ class FleetVehicle(models.Model):
         store=True,
     )
 
+    rental_start_date = fields.Date(
+        string='Delivery Date',
+        compute='_compute_rental_status',
+        store=True,
+    )
+
 
     @api.depends('log_contracts.state')
     def _compute_rental_status(self):
@@ -113,10 +119,12 @@ class FleetVehicle(models.Model):
             if running_contract:
                 vehicle.rental_status = 'In Rent'
                 vehicle.rental_return_date = running_contract.rent_end_date
+                vehicle.rental_start_date = running_contract.rent_start_date
             else:
                 vehicle.rental_status = 'Available'
                 vehicle.rental_return_date = False
-        
+                vehicle.rental_start_date = False
+
     reservation_id = fields.Many2one(
         'vehicle.reservation',
         string='Reservation',
